d017b246db88772df0894f75b9bb22b56d3457fb
Silpashree <silpashree@bootlabstech.com>
2022-10-07 11:46:44 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "dataset_name" {
+  type        = string
+  description = "the name of the dataset"
+}
+variable "region" {
+  type        = string
+  description = "the region of the dataset"
+}
