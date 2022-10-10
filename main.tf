7c4ffcab01ec244e46e9d64c77f72de4c414d57d
Silpashree <silpashree@bootlabstech.com>
2022-10-10 12:11:11 +0530

    added project fix

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "google_project_service" "vertexaiapi" {
-  project = module.project.project_id
+  project = var.project_id
   service = "aiplatform.googleapis.com"
 }
 resource "google_vertex_ai_dataset" "dataset" {
   depends_on            = [google_project_service.vertexaiapi]
-  project               = module.project.project_id
+  project               = var.project_id
   display_name          = var.dataset_name
   metadata_schema_uri   = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
   region                = var.region
--- variables.tf
+++ variables.tf
@@ -6,3 +6,7 @@ variable "region" {
   type        = string
   description = "the region of the dataset"
 }
+variable "project_id" {
+  type        = string
+  description = "this is the id of the project."
+}
