1a766a696fe2be65c9de17453d93715dc017f9c7
Silpashree Acharya <silpashree@bootlabstech.com>
2023-06-10 12:26:48 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -2,11 +2,44 @@ resource "google_project_service" "vertexaiapi" {
   project = var.project_id
   service = "aiplatform.googleapis.com"
 }
-resource "google_vertex_ai_dataset" "dataset" {
-  depends_on            = [google_project_service.vertexaiapi]
-  project               = var.project_id
-  display_name          = var.dataset_name
-  metadata_schema_uri   = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
-  region                = var.region
+resource "google_project_service" "notebooks" {
+  project            = var.project_id
+  provider           = google
+  service            = "notebooks.googleapis.com"
+  disable_on_destroy = false
 }
+
+resource "google_notebooks_instance" "basic_instance" {
+  project                = var.project_id
+  provider               = google
+  name                   = "notebooks-instance-basic"
+  location               = var.zone
+  machine_type           = "n1-standard-1"
+  service_account        = var.service_account
+  service_account_scopes = var.service_account_scopes
+  boot_disk_type         = var.boot_disk_type
+  boot_disk_size_gb      = var.boot_disk_size_gb
+  data_disk_type         = var.data_disk_type
+  data_disk_size_gb      = var.data_disk_size_gb
+  no_remove_data_disk    = var.no_remove_data_disk
+  # kms_key                = var.kms_key
+  no_public_ip           = var.no_public_ip
+  no_proxy_access        = var.no_proxy_access
+  network                = var.network
+  subnet                 = var.subnetwork
+
+  vm_image {
+    project      = "deeplearning-platform-release"
+    image_family = "tf-ent-2-9-cu113-notebooks"
+  }
+  # accelerator_config {
+  #   type       = var.accelerator_type
+  #   core_count = var.accelerator_core_count
+  # }
+
+  depends_on = [
+    google_project_service.notebooks
+  ]
+}
+
 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,69 @@
-variable "dataset_name" {
-  type        = string
-  description = "the name of the dataset"
-}
 variable "region" {
   type        = string
   description = "the region of the dataset"
+}
+variable "zone" {
+  type        = string
+  description = "the zone of the vertex ai notebook"
 }
 variable "project_id" {
   type        = string
   description = "this is the id of the project."
 }
+variable "service_account" {
+  type        = string
+  description = "service_account"
+}
+variable "service_account_scopes" {
+  type        = list(string)
+  description = "service_account_scopes"
+}
+variable "boot_disk_size_gb" {
+  type        = number
+  description = "boot_disk_size_gb"
+}
+variable "boot_disk_type" {
+  type        = string
+  description = "boot_disk_type"
+}
+variable "data_disk_type" {
+  type        = string
+  description = "data_disk_type"
+}
+variable "data_disk_size_gb" {
+  type        = number
+  description = "data_disk_size_gb"
+}
+variable "no_remove_data_disk" {
+  type        = bool
+  description = "no_remove_data_disk"
+}
+# variable "kms_key" {
+#   type        = string
+#   description = "kms_key"
+# }
+variable "no_proxy_access" {
+  type        = bool
+  description = "no_proxy_access"
+}
+variable "no_public_ip" {
+  type        = bool
+  description = "no_public_ip"
+}
+variable "network" {
+  type        = string
+  description = "network"
+}
+variable "subnetwork" {
+  type        = string
+  description = "subnetwork"
+}
+# variable "accelerator_core_count" {
+#   type        = number
+#   description = "accelerator_core_count"
+# }
+# variable "accelerator_type" {
+#   type        = string
+#   description = "accelerator_type"
+# }
+
